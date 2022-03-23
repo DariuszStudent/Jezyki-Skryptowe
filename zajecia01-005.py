335fc8cd4b7c59750bc0e0841d9991071c4ad31b
Dariusz Wroniak <dariuszwr@gmail.com>
2022-03-23 20:18:30 +0100

    Zajecia01-005

--- zajecia01-005.py
+++ zajecia01-005.py
@@ -1,6 +1,26 @@
 # =============================================
 # M1Z5
 
-
+zmienna01 = 5E-1
+zmienna02 = (3==3)/2
+zmienna03 = (3==3)/2.0
+zmienna04 = 1/2
+zmienna05 = float(1)/2
+zmienna06 = 5.
+#zmienna07 = 0.5L
+zmienna08 = 5*((2>3)<2)
+#zmienna09 = 1.0>>2
+#zmienna10 = 1 / ( ( ' a'=='A' |2 <3)+1))
+print("liczba z działania 5E-1 to: ", zmienna01)
+print("liczba z działania (3==3)/2 to: ", zmienna02)
+print("liczba z działania (3==3)/2.0 to: ", zmienna03)
+print("liczba z działania 1/2to: ", zmienna04)
+print("liczba z działania 1/2to: ", zmienna04)
+print("liczba z działania float(1)/2 to: ", zmienna05)
+print("liczba z działania 5. to: ", zmienna06)
+print("liczba z działania 0.5L to: error")
+print("liczba z działania 5*((2>3)<2)to: ", zmienna08)
+print("liczba z działania 1.0>>2 to: error")
+print("liczba z działania 1 / ( ( ' a'=='A' |2 <3)+1) to: error")
 
 # =============================================
