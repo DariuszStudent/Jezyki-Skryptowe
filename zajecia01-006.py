85306c5242e005ebdcacdfbbe0cc20bcf1097456
Dariusz Wroniak <dariuszwr@gmail.com>
2022-03-28 18:08:37 +0200

    zajecia01-007

--- zajecia01-006.py
+++ zajecia01-006.py
@@ -13,15 +13,17 @@
 # Przyjmuje ona taki typ, jaki jest w przypisanej do niej wartości.
 
 a = 3
-print('a jest to zmienna "a = 3": ', type(a))
+print('-a jest to zmienna "a = 3": ', type(a))
 b = 3.5
-print('b jest to zmienna "b = 3.5": ', type(b))
+print('-b jest to zmienna "b = 3.5": ', type(b))
 c = "ala ma kota"
-print('c jest to zmienna "c = ala ma kota": ', type(c))
+print('-c jest to zmienna "c = ala ma kota": ', type(c))
 # d = d + 1 #niepoprawne
 
 e = 5*2.0
-print('wynik działania: "e = 5*2.0" to: ', e)
-print('e jest to zmienna typu: ', type(e))
+print('-wynik działania: "e = 5*2.0" to: ', e)
+print('-e jest to zmienna typu: ', type(e))
+print('-konwertujemy zmienną typu float to int: "int(round(e))\n'
+      'round przy okazji zaokrągla ', int(round(e)))
 
 # =============================================
--- zajecia01-007.py
+++ zajecia01-007.py
@@ -1,11 +1,11 @@
 # =============================================
 # M1Z7
 
-PRZELICZNIK = 1.68
+PRZELICZNIK = 0.62137
 print("Podaj km do przeliczenia na mile: ", end="")
 km = int(input())
-mile = km / PRZELICZNIK
-print("Wynik = ", mile)
+mile = km * PRZELICZNIK
+print("Wynik", km, "km to:", round(mile, 2), "mil")
 
 
 # =============================================
