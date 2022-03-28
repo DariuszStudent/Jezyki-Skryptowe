5b0cb0e83ac34ee5f5145532a69e40e91a777dfe
Dariusz Wroniak <dariuszwr@gmail.com>
2022-03-28 19:50:51 +0200

    zajecia01-prawie koniec

--- zajecia01-012.py
+++ zajecia01-012.py
@@ -1,7 +1,15 @@
 # =============================================
 # M1Z12
 
+import datetime
 
+today = datetime.date.today()
+endWorldWarII = datetime.date(1945, 5, 8)
 
+print("Dzisiejsza data to:", today)
+print("Data zakończenia drugiej wojny światowej:", endWorldWarII)
+difference = today - endWorldWarII
+
+print("Od drugiej wojny światowej minęło:", difference.days)
 
 # =============================================
