37041e44fa30fc5a6207dc8a5e5df865a73b3d59
Dariusz Wroniak <dariuszwr@gmail.com>
2022-03-28 18:42:18 +0200

    zajecia01-011

--- zajecia01-011.py
+++ zajecia01-011.py
@@ -1,7 +1,11 @@
 # =============================================
 # M1Z11
+from datetime import datetime, timedelta
 
-
-
+today = datetime.now()
+print("Aktualna data to:", today.year)
+daysInput = int(input("Podaj ilość dni: "))
+today = today + timedelta(days=daysInput)
+print("Za", daysInput, "dni będzie rok", today.year)
 
 # =============================================
