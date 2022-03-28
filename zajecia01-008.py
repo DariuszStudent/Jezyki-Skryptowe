851ab50b4e851ccac8ad832b2ad6f16ebeb513e0
Dariusz Wroniak <dariuszwr@gmail.com>
2022-03-28 18:17:36 +0200

    zajecia01-008

--- zajecia01-008.py
+++ zajecia01-008.py
@@ -1,7 +1,9 @@
 # =============================================
-# M1Z7
+# M1Z8
 
-
-
+tCelsjusz = input("Podaj wartość stopni Celsjusza aby przeliczyć na Fahrenheita: ")
+tCelsjusz = float(tCelsjusz)
+tCels = 9/5.0 * tCelsjusz + 32
+print("Temperatury w stopniach Fahrenheita to: ", round(tCels, 2))
 
 # =============================================
