a57c0e9d69d4509ba5c05fc89fa407623d519374
Dariusz Wroniak <dariuszwr@gmail.com>
2022-05-20 21:18:44 +0200

    zajecia02-001

---
+++ Zajecia02/zajecia02-001.py
@@ -0,0 +1,18 @@
+# =============================================
+# M2Z00
+
+print("Jeśli liczba jest większa od zera program wyświetli TAK, jeśli nie wyświetli NIE")
+userNumber = input("Podaj liczbę: ")
+try:
+    userNumber = int(userNumber)
+    if userNumber == 0:
+        print("Podana liczba jest równa 0")
+    elif userNumber > 0:
+        print("Podana liczba jest większa od zera")
+    else:
+        print("Liczba jest mniejsza od zera")
+except:
+    print("Nie podałeś liczby całkowitej, tylko: {}"
+          "\n Koniec programu".format(userNumber))
+
+# =============================================
