c08120bc79b2af469029e601be75793e180a4078
Dariusz Wroniak <dariuszwr@gmail.com>
2022-05-29 22:10:52 +0200

    Zajecia02-008

--- Zajecia02/zajecia02-008.py
+++ Zajecia02/zajecia02-008.py
@@ -1,17 +1,31 @@
 # =============================================
 # M2Z08
 
-n = 60
+import Helpers as helpers
 
-i = 128
+exitWhile = False
+while not exitWhile:
+    print("################################################################################\n")
+    while not exitWhile:
+        userVar = input("Podaj liczbę naturalną nie większą niż 128, wyświętlę ją jako wartość binarna: ")
+        liczba, exitWhile = helpers.userVariableINT(userVar)
+    if liczba > 128 or liczba < 1:
+        print("Podana liczba nie jest z zadanego przedziału")
+        helpers.exitProgram()
+        exitWhile = False
+        continue
 
-while(i > 0):
-    if (n & i > 0):
-        print("1", end="")
-    else:
-        print("0", end="")
-    i = i >> 1
+    i = 128
+    print("Liczba {} to liczba w zapisie binarnym:".format(liczba), end=" ")
+    while i > 0:
+        if liczba & i > 0:
+            print("1", end="")
+        else:
+            print("0", end="")
+        i = i >> 1
+    print()
 
-
+    helpers.exitProgram()
+    exitWhile = False
 
 # =============================================
