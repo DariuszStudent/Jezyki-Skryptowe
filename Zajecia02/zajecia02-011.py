31063f76a40fbcf4e34d9fa5faf68093f875bb47
Dariusz Wroniak <dariuszwr@gmail.com>
2022-05-30 22:08:09 +0200

    Zajecia02-011

--- Zajecia02/zajecia02-011.py
+++ Zajecia02/zajecia02-011.py
@@ -1,25 +1,37 @@
 # =============================================
 # M2Z11
+import Helpers as helpers
 
 znak = ' . '
+exitWhile = False
+while not exitWhile:
+    print("################################################################################\n")
+    while not exitWhile:
+        userVar = input("Podaj dlugosc macierzy kwadratowej <int>x<int>: ")
+        ilosc, exitWhile = helpers.userVariableINT(userVar)
+    userInput = input("Podaj znak, który chcesz użyć jako przekątna "
+                        "(program przechwyci tylko pierwszy znak): ")
+    znakUser = userInput[0]
 
-for i in range(0, 10):
-    for j in range(0, 10):
-        if (i == j):
-            print(' 0 ', end='')
-        else:
-            print(znak, end='')
+    for i in range(0, ilosc):
+        for j in range(0, ilosc):
+            if i == j:
+                print(' {} '.format(znakUser), end='')
+            else:
+                print(znak, end='')
+        print()
+
     print()
 
-print()
+    for i in range(0, ilosc):
+        for j in range(0, ilosc):
+            if j + i == ilosc - 1:
+                print(' {} '.format(znakUser), end='')
+            else:
+                print(znak, end='')
+        print()
 
-for i in range(0, 10):
-    for j in range(0, 10):
-        if (j + i == 9):
-            print(' 0 ', end='')
-        else:
-            print(znak, end='')
-    print()
-
+    helpers.exitProgram()
+    exitWhile = False
 
 # =============================================
