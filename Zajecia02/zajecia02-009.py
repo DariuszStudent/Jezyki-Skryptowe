2e776df111ac58d1bcc44bbaff47c565ecb8965f
Dariusz Wroniak <dariuszwr@gmail.com>
2022-05-30 21:31:31 +0200

    Zajecia02-009

--- Zajecia02/zajecia02-009.py
+++ Zajecia02/zajecia02-009.py
@@ -1,12 +1,30 @@
 # =============================================
 # M2Z09
 
-odp = 't'
+import Helpers as helpers
 
-while (odp == 't'):
-    a, b, c, = input("Podaj trzy zmienne oddzielone spacjami.").split(' ')
-    print("Podano:", a, b, c)
-    odp = input("Czy kontynuowac? [t/n]? ")
-    odp = odp.lower()
+exitWhile = False
+while not exitWhile:
+    print("################################################################################\n")
+    while not exitWhile:
+        try:
+            userVar1, userVar2, userVar3 = input("Podaj trzy liczby oddzielone spacjami: ").split(' ')
+        except Exception as e:
+            print(e)
+            helpers.exitProgram()
+            continue
+        userNumber1, exitWhile = helpers.userVariableFloat(userVar1)
+        if not exitWhile:
+            continue
+        userNumber2, exitWhile = helpers.userVariableFloat(userVar2)
+        if not exitWhile:
+            continue
+        userNumber3, exitWhile = helpers.userVariableFloat(userVar3)
+
+    lista = [userNumber1, userNumber2, userNumber3]
+    print("Najmniejsza podana liczba to: {:.2f}".format(min(lista)))
+
+    helpers.exitProgram()
+    exitWhile = False
 
 # =============================================
