3f76fdbc5de2c5e0ac75995bcf5acb011791aa30
Dariusz Wroniak <dariuszwr@gmail.com>
2022-06-06 21:14:59 +0200

    Zajecia02-017

--- Zajecia02/zajecia02-017.py
+++ Zajecia02/zajecia02-017.py
@@ -1,10 +1,63 @@
 # =============================================
 # M2Z17
-from math import sqrt
-X = 123
+import Helpers as helpers
+import random as rnd
+import time
+import sympy
+import math
 
-if sqrt.isPrime(X):
+
+def isprime(num):
+    for n in range(2, int(num**0.5)+1):
+        if num % n == 0:
+            return False
+    return True
 
 
+def isprimeMath(num):
+    a = 2
+    while a <= math.sqrt(num):
+        if num % a < 1:
+            return False
+        a = a+1
+    return num > 1
+
+
+exitWhile = False
+while not exitWhile:
+    print("################################################################################\n")
+    while not exitWhile:
+        userVar = input("Podaj początek przedziału: ")
+        intervalFirst, exitWhile = helpers.userVariableINT(userVar)
+    exitWhile = False
+    while not exitWhile:
+        userVar = input("Podaj koniec przedziału: ")
+        intervalLast, exitWhile = helpers.userVariableINT(userVar)
+    if intervalFirst > intervalLast:
+        print("Podane wartości są błędne, pierwsza liczba musi mniejsza od drugiej liczby.")
+        helpers.exitProgram()
+        exitWhile = False
+        continue
+
+    randomNumber = rnd.randrange(intervalFirst, intervalLast + 1)
+    print(randomNumber)
+
+    start1 = time.time()
+    print("DEF- isprime :Czy liczba {} jest liczbą pierwszą: {}".format(randomNumber, isprime(randomNumber)))
+    end1 = time.time()
+    print(end1 - start1)
+
+    start2= time.time()
+    print("Math :Czy liczba {} jest liczbą pierwszą: {}".format(randomNumber, isprimeMath(randomNumber)))
+    end2 = time.time()
+    print(end2 - start2)
+
+    start3 = time.time()
+    print("sympry :Czy liczba {} jest liczbą pierwszą: {}".format(randomNumber, sympy.isprime(randomNumber)))
+    end3 = time.time()
+    print(end3 - start3)
+
+    helpers.exitProgram()
+    exitWhile = False
 
 # =============================================
