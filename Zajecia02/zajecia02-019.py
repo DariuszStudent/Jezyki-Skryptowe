88212b305918aac042161ef5479f9173c24dd8f0
Dariusz Wroniak <dariuszwr@gmail.com>
2022-06-08 19:57:53 +0200

    zajecia02-019

---
+++ Zajecia02/zajecia02-019.py
@@ -0,0 +1,24 @@
+# =============================================
+# M1Z14
+import turtle as t
+
+t.penup()
+t.goto(-50, 50)
+t.pendown()
+for i in range(0, 4):
+    t.forward(100)
+    t.right(90)
+t.delay(200)
+t.clear()
+t.penup()
+t.goto(-22, 22)
+t.pendown()
+for i in range(0, 8):
+    t.forward(30)
+    t.right(45)
+
+t.Screen().exitonclick()
+
+
+
+# =============================================
