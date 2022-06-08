d92604612d1ec9470304c37b660ef7b3a5387151
Dariusz Wroniak <dariuszwr@gmail.com>
2022-06-08 19:35:16 +0200

    zajecia02-018

---
+++ Zajecia02/zajecia02-018.py
@@ -0,0 +1,46 @@
+# =============================================
+# M2Z16
+import Helpers as helpers
+import random as rnd
+
+user = 0
+computer = 0
+
+exitWhile = False
+while not exitWhile:
+    print("################################################################################\n")
+    while not exitWhile:
+        userVar = input("Określ ilość rzutów kostką, wpisz liczbę: ")
+        userInput, exitWhile = helpers.userVariableINT(userVar)
+    oczek1 = oczek2 = oczek3 = oczek4 = oczek5 = oczek6 = 0
+
+    for i in range(1, userInput + 1):
+        los = rnd.randrange(1, 7)
+        print("Rzut nr: {:>3} Wyrzucono: {}".format(i, los))
+        if los == 1:
+            oczek1 += 1;
+        if los == 2:
+            oczek2 += 1;
+        if los == 3:
+            oczek3 += 1;
+        if los == 4:
+            oczek4 += 1;
+        if los == 5:
+            oczek5 += 1;
+        if los == 6:
+            oczek6 += 1;
+
+    print("Statystyka rzutów:")
+    print("Oczek:  1 ilość: {:>2}".format(oczek1))
+    print("Oczek:  2 ilość: {:>2}".format(oczek2))
+    print("Oczek:  3 ilość: {:>2}".format(oczek3))
+    print("Oczek:  4 ilość: {:>2}".format(oczek4))
+    print("Oczek:  5 ilość: {:>2}".format(oczek5))
+    print("Oczek:  6 ilość: {:>2}".format(oczek6))
+
+
+
+    helpers.exitProgram()
+    exitWhile = False
+
+# =============================================
