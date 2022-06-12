4b35639d3cfedf81ac0d56c78cd41f1e2d32e971
Grzegorz0831 <zuromski.grzegorz@wp.pl>
2022-06-12 12:35:53 +0200

    uzupełnienie zadań

--- Zajecia03/zajecia03-06.py
+++ Zajecia03/zajecia03-06.py
@@ -1,2 +1,22 @@
-w = "fkdsnfgjk lsdhnfklsdhfkj sdklfjdskl"
-#statystyka
+# =============================================
+# M3Z06
+
+tekst = input("Wpisz swój tekst:")
+
+liczba_liter = 0
+ilosc_liczb = 0
+ilosc_znakow = 0
+
+for i in tekst:
+    if i.isalpha():
+        liczba_liter += 1
+    elif i.isdigit():
+        ilosc_liczb += 1
+    else:
+        ilosc_znakow += 1
+
+print("Liczba liter to: ", liczba_liter)
+print("Ilość liczb to: ", ilosc_liczb)
+print("Liczba znaków to: ", ilosc_znakow)
+
+# =============================================
