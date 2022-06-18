9f68768a36eb5ae4d3339bc86d3497aed758862b
Grzegorz0831 <zuromski.grzegorz@wp.pl>
2022-06-18 09:06:26 +0200

    Szyf Cezara

--- Zajecia04/zajecia04-07.py
+++ Zajecia04/zajecia04-07.py
@@ -1,18 +1,22 @@
 # =============================================
 # M4Z07
-import Helper as helper
+tekst = input("Podaj tekst do zakodowania: \n")
+klucz = int(input("Jaki ma być klucz kodu?: \n"))
+
+def szyfr_cezara(tekst, klucz):
+    wynik = ""
+
+    for i in range(len(tekst)):
+        litera = tekst[i]
+        if (litera.isupper()):
+            wynik += chr((ord(litera) + klucz - 64) % 26 + 64)
+        else:
+            wynik += chr((ord(litera) + klucz - 96) % 26 + 96)
+    return wynik
 
 
-def main(args):
-    while True:
-        print("################################################################################\n")
-
-        helper.exitProgram()
-
-
-if __name__ == '__main__':
-    import sys
-
-    sys.exit(main(sys.argv))
+print("Tekst : ", tekst)
+print("Klucz : ", str(klucz))
+print("Kod zezara: ", szyfr_cezara(tekst, klucz))
 
 # =============================================
