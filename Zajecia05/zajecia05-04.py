2966e95b74c690900a40d4a294738fb42eb62a92
Dariusz Wroniak <dariuszwr@gmail.com>
2022-06-18 14:34:39 +0200

    Zajecia05-04

--- Zajecia05/zajecia05-04.py
+++ Zajecia05/zajecia05-04.py
@@ -5,7 +5,15 @@ import Helper as helper
 
 def main(args):
     while True:
+        licznik = 0
         print("################################################################################\n")
+        alfabet = "abcdefghijklmnopqrstuvwxyz"
+        for i in alfabet:
+            print(" {} => {}".format(i, i.upper()), end="")
+            licznik += 1
+            if licznik == 5:
+                print()
+                licznik = 0
 
         helper.exitProgram()
 
